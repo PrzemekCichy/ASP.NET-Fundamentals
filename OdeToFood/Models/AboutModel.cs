b4efd5fa6b410f9fa66afe17e465c0c4f24f2250
przemyslaw.cichy <przemyslaw.cichy@PRZEMYSLAW-PC>
2016-06-16 12:19:13 +0100

    Add project files.

---
+++ OdeToFood/Models/AboutModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFood.Models
+{
+    public class AboutModel
+    {
+        public string Location { get; internal set; }
+        public string Name { get; set; }
+    }
+}
